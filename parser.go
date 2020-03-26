ea5bdf0dc450e7bc278e332c71f3ed81ef0b303f
unknown <hth0919@gmail.com>
2020-03-26 16:25:36 +0900

    3rd commit

--- parser.go
+++ parser.go
@@ -1,12 +1,15 @@
 package tsdbParser
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strconv"
+)
 
 func JsonUnmarshaller (jsonfile []byte) *Metric {
 	mt := &Metric{
 		MetricName: "",
 		Tags: map[string]string{},
-		Dps: map[int64]float64{},
+		Dps: map[string]float64{},
 	}
 	err :=json.Unmarshal(jsonfile, mt)
 	if err != nil {
@@ -17,13 +20,19 @@ func JsonUnmarshaller (jsonfile []byte) *Metric {
 }
 
 func GetLast(input *Metric) float64 {
-	var last int64
+	var last int
 	last = 0
 	for k := range input.Dps {
-		if k>last {
-			last = k
+		i,err := strconv.Atoi(k)
+		if err != nil {
+			panic(err)
+		}
+		if i>last {
+			last = i
 		}
 	}
 
-	return input.Dps[last]
+	l := strconv.Itoa(last)
+
+	return input.Dps[l]
 }
